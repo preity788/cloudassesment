ce853fcd5e21f2b19e883077e57b61eb0b2c9ca1
preity788 <preityahir7@gmail.com>
2020-10-22 19:16:29 +0530

    My First Commit

---
+++ HelloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/*
+      Maintained By: Preity Yadav
+
+*/
+
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
